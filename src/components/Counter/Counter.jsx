fa4d739342551f2800a0af132c75bced35c47927
ganeshbachu61 <ganeshbachu61@gmail.com>
2025-01-15 23:08:48 +0530

    Added Routing

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -2,17 +2,13 @@ import react, { useState } from 'react'
 
 const Counter = () => {
     const [data,setData] = useState(0);
-    const [toggle,setToggle] = useState(false);
-    return(
+    return (
         <div>
             <h1>Counter Project</h1>
             <p>Counter:{data}</p>
-            <button onClick={()=> setData(data+1)}>Increment</button>
-            <button onClick={()=> setData(data-1)}>Decrement</button>
-            <h1>Toggle Switch</h1>
-            <button onClick={()=>setToggle(!toggle)}>
-                {toggle?'on':'off'}
-            </button>
+            {/* increment and decrement logic */}
+            <button onClick={()=>setData(data+1)}>Increment</button>
+            <button onClick={()=>setData(data-1)}>Decrement</button>
         </div>
     )
 }
